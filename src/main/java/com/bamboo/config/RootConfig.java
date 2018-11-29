8419ea00f2df8a8e4e976c9e30ba0021a32ac1a0
leafy <icarus8050@naver.com>
2018-11-29 18:35:24 +0900

    데이터베이스 설정
    ,MySQL, HikariCP, MyBatis 설정 완료

--- src/main/java/com/bamboo/config/RootConfig.java
+++ src/main/java/com/bamboo/config/RootConfig.java
@@ -1,8 +1,40 @@
 package com.bamboo.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
+@MapperScan(basePackages = {"com.bamboo.mapper"})
 public class RootConfig {
 
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		
+		hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mysql://13.125.69.50:56375");
+		hikariConfig.setUsername("root");
+		hikariConfig.setPassword("1234");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		SqlSessionFactoryBean sqlSessionFactory =
+				new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+	
 }
---
+++ src/main/java/com/bamboo/domain/User_DTO.java
@@ -0,0 +1,12 @@
+package com.bamboo.domain;
+
+import lombok.Data;
+
+@Data
+public class User_DTO {
+	String p_uid;
+	int f_code;
+	String name;
+	String alias;
+	String password;
+}
